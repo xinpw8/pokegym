bc791c84e65ae3110c942e91d6cc9966c84e28c6
xinpw8 <scab.nft+github@gmail.com>
2024-01-17 03:13:39 -0600

    2nd

---
+++ pokegym/environment_backup.py
@@ -0,0 +1,299 @@
+from pdb import set_trace as T
+from gymnasium import Env, spaces
+import numpy as np
+import os
+
+from pokegym.pyboy_binding import (ACTIONS, make_env, open_state_file,
+    load_pyboy_state, run_action_on_emulator)
+from pokegym import ram_map, game_map, data
+
+
+def play():
+    '''Creates an environment and plays it'''
+    env = Environment(rom_path='pokemon_red.gb', state_path=None, headless=False,
+        disable_input=False, sound=False, sound_emulated=False, verbose=True
+    )
+
+    env.reset()
+    env.game.set_emulation_speed(1)
+
+    # Display available actions
+    print("Available actions:")
+    for idx, action in enumerate(ACTIONS):
+        print(f"{idx}: {action}")
+
+    # Create a mapping from WindowEvent to action index
+    window_event_to_action = {
+        'PRESS_ARROW_DOWN': 0,
+        'PRESS_ARROW_LEFT': 1,
+        'PRESS_ARROW_RIGHT': 2,
+        'PRESS_ARROW_UP': 3,
+        'PRESS_BUTTON_A': 4,
+        'PRESS_BUTTON_B': 5,
+        'PRESS_BUTTON_START': 6,
+        'PRESS_BUTTON_SELECT': 7,
+        # Add more mappings if necessary
+    }
+
+    while True:
+        # Get input from pyboy's get_input method
+        input_events = env.game.get_input()
+        env.game.tick()
+        env.render()
+        if len(input_events) == 0:
+            continue
+
+        for event in input_events:
+            event_str = str(event)
+            if event_str in window_event_to_action:
+                action_index = window_event_to_action[event_str]
+                observation, reward, done, _, info = env.step(
+                    action_index, fast_video=False)
+
+                # Check for game over
+                if done:
+                    print(f"{done}")
+                    break
+
+                # Additional game logic or information display can go here
+                print(f"new Reward: {reward}\n")
+
+class Base:
+    def __init__(self, rom_path='pokemon_red.gb',
+            state_path=None, headless=True, quiet=False, **kwargs):
+        '''Creates a PokemonRed environment'''
+        if state_path is None:
+            state_path = __file__.rstrip('environment.py') + 'has_pokedex_nballs.state'
+
+        self.game, self.screen = make_env(
+            rom_path, headless, quiet, **kwargs)
+
+        self.initial_state = open_state_file(state_path)
+        self.headless = headless
+
+        R, C = self.screen.raw_screen_buffer_dims()
+        self.observation_space = spaces.Dict({
+        'image': spaces.Box(low=0, high=255, shape=self.output_full, dtype=np.uint8),
+        'vector': spaces.Box(low=-1, high=1, shape=self.output_vector_shape, dtype=np.float32),
+        'map_ids': spaces.Box(low=0, high=255, shape=(1,), dtype=np.uint8),
+        'item_ids': spaces.Box(low=0, high=255, shape=(20,), dtype=np.uint8),
+        'item_quantity': spaces.Box(low=-1, high=1, shape=(20, 1), dtype=np.float32),
+        'poke_ids': spaces.Box(low=0, high=255, shape=(12,), dtype=np.uint8),
+        'poke_type_ids': spaces.Box(low=0, high=255, shape=(12, 2), dtype=np.uint8),
+        'poke_move_ids': spaces.Box(low=0, high=255, shape=(12, 4), dtype=np.uint8),
+        'poke_move_pps': spaces.Box(low=-1, high=1, shape=(12, 4, 2), dtype=np.float32),
+        # 'poke_all': spaces.Box(low=-1, high=1, shape=(12, self.n_pokemon_features), dtype=np.float32),
+        'event_ids': spaces.Box(low=0, high=2570, shape=(10,), dtype=np.int16),
+        'event_step_since': spaces.Box(low=-1, high=1, shape=(10, 1), dtype=np.float32),
+        # 'in_battle_mask': spaces.Box(low=0, high=1, shape=(1,), dtype=np.float32),
+    })
+        self.action_space = spaces.Discrete(len(ACTIONS))
+
+    def reset(self, seed=None, options=None):
+        '''Resets the game. Seeding is NOT supported'''
+        load_pyboy_state(self.game, self.initial_state)
+        return self.screen.screen_ndarray(), {}
+
+    def render(self):
+        return self.screen.screen_ndarray()
+
+    def step(self, action):
+        run_action_on_emulator(self.game, self.screen, ACTIONS[action], self.headless)
+        return self.render(), 0, False, False, {}
+    
+    update observation_space
+    update observation 
+    update neural network
+    
+
+    # self.observation_space = spaces.Box(low=0, high=255, shape=self.output_full, dtype=np.uint8)
+    self.observation_space = spaces.Dict({
+        'image': spaces.Box(low=0, high=255, shape=self.output_full, dtype=np.uint8),
+        'vector': spaces.Box(low=-1, high=1, shape=self.output_vector_shape, dtype=np.float32),
+        'map_ids': spaces.Box(low=0, high=255, shape=(1,), dtype=np.uint8),
+        'item_ids': spaces.Box(low=0, high=255, shape=(20,), dtype=np.uint8),
+        'item_quantity': spaces.Box(low=-1, high=1, shape=(20, 1), dtype=np.float32),
+        'poke_ids': spaces.Box(low=0, high=255, shape=(12,), dtype=np.uint8),
+        'poke_type_ids': spaces.Box(low=0, high=255, shape=(12, 2), dtype=np.uint8),
+        'poke_move_ids': spaces.Box(low=0, high=255, shape=(12, 4), dtype=np.uint8),
+        'poke_move_pps': spaces.Box(low=-1, high=1, shape=(12, 4, 2), dtype=np.float32),
+        # 'poke_all': spaces.Box(low=-1, high=1, shape=(12, self.n_pokemon_features), dtype=np.float32),
+        'event_ids': spaces.Box(low=0, high=2570, shape=(10,), dtype=np.int16),
+        'event_step_since': spaces.Box(low=-1, high=1, shape=(10, 1), dtype=np.float32),
+        # 'in_battle_mask': spaces.Box(low=0, high=1, shape=(1,), dtype=np.float32),
+    })
+
+    def close(self):
+        self.game.stop(False)
+
+
+class Environment(Base):
+    def __init__(self, rom_path='pokemon_red.gb',
+            state_path=None, headless=True, quiet=False, verbose=False, **kwargs):
+        super().__init__(rom_path, state_path, headless, quiet, **kwargs)
+        self.counts_map = np.zeros((444, 436))
+        self.verbose = verbose
+        self.reset_count = 0
+
+    #def reset(self, seed=None, options=None, max_episode_steps=20480, reward_scale=4.0):
+    def reset(self, seed=None, options=None, max_episode_steps=2048, reward_scale=4.0):
+        '''Resets the game. Seeding is NOT supported'''
+        load_pyboy_state(self.game, self.initial_state)
+
+        self.time = 0
+        self.reset_count += 1
+        self.max_episode_steps = max_episode_steps
+        self.reward_scale = reward_scale
+         
+        self.max_events = 0
+        self.max_level_sum = 0
+        self.max_opponent_level = 0
+
+        self.seen_coords = set()
+        self.seen_maps = set()
+
+        self.death_count = 0
+        self.total_healing = 0
+        self.last_hp = 1.0
+        self.last_party_size = 1
+        self.last_reward = None
+
+        return self.render()[::2, ::2], {}
+
+    def step(self, action, fast_video=True):
+        run_action_on_emulator(self.game, self.screen, ACTIONS[action],
+            self.headless, fast_video=fast_video)
+        self.time += 1
+
+        # Exploration reward
+        r, c, map_n = ram_map.position(self.game)
+        self.seen_coords.add((r, c, map_n))
+        self.seen_maps.add(map_n)
+        exploration_reward = 0.01 * len(self.seen_coords)
+        glob_r, glob_c = game_map.local_to_global(r, c, map_n)
+        try:
+            self.counts_map[glob_r, glob_c] += 1
+        except:
+            pass
+
+        # Level reward
+        party, party_size, party_levels = ram_map.party(self.game)
+        self.max_level_sum = max(self.max_level_sum, sum(party_levels))
+        if self.max_level_sum < 30:
+            level_reward = 1 * self.max_level_sum
+        else:
+            level_reward = 30 + (self.max_level_sum - 30)/4
+
+        # Healing and death rewards
+        hp = ram_map.hp(self.game)
+        hp_delta = hp - self.last_hp
+        party_size_constant = party_size == self.last_party_size
+
+        # Only reward if not reviving at pokecenter
+        if hp_delta > 0 and party_size_constant and not self.is_dead:
+            self.total_healing += hp_delta
+
+        # Dead if hp is zero
+        if hp <= 0 and self.last_hp > 0:
+            self.death_count += 1
+            self.is_dead = True
+        elif hp > 0.01: # TODO: Check if this matters
+            self.is_dead = False
+
+        # Update last known values for next iteration
+        self.last_hp = hp
+        self.last_party_size = party_size
+
+        # Set rewards
+        healing_reward = self.total_healing
+        death_reward = -0.05 * self.death_count
+
+        # Opponent level reward
+        max_opponent_level = max(ram_map.opponent(self.game))
+        self.max_opponent_level = max(self.max_opponent_level, max_opponent_level)
+        opponent_level_reward = 0.2 * self.max_opponent_level
+
+        # Badge reward
+        badges = ram_map.badges(self.game)
+        badges_reward = 5 * badges
+
+        # Event reward
+        events = ram_map.events(self.game)
+        self.max_events = max(self.max_events, events)
+        event_reward = self.max_events
+
+        money = ram_map.money(self.game)
+
+        reward = self.reward_scale * (event_reward + level_reward + 
+            opponent_level_reward + death_reward + badges_reward +
+            healing_reward + exploration_reward)
+
+        # Subtract previous reward
+        # TODO: Don't record large cumulative rewards in the first place
+        if self.last_reward is None:
+            reward = 0
+            self.last_reward = 0
+        else:
+            nxt_reward = reward
+            reward -= self.last_reward
+            self.last_reward = nxt_reward
+
+        info = {}
+        done = self.time >= self.max_episode_steps
+        if done:
+            pokemon_info = data.pokemon_l(self.game)
+            x, y ,map_n = ram_map.position(self.game)
+            reset = self.reset_count
+            pokemon = []
+            for p in pokemon_info:
+                pokemon.append({
+                    'slot': p['slot'],
+                    'name': p['name'],
+                    'level': p['level'],
+                    'moves': p['moves'],
+                })
+
+            info = {
+                'reward': {
+                    'delta': reward,
+                    'event': event_reward,
+                    'level': level_reward,
+                    'opponent_level': opponent_level_reward,
+                    'death': death_reward,
+                    'badges': badges_reward,
+                    'healing': healing_reward,
+                    'exploration': exploration_reward,
+                },
+                'maps_explored': len(self.seen_maps),
+                'party_size': party_size,
+                'highest_pokemon_level': max(party_levels),
+                'total_party_level': sum(party_levels),
+                'deaths': self.death_count,
+                'badge_1': float(badges == 1),
+                'badge_2': float(badges > 1),
+                'event': events,
+                'money': money,
+                'pokemon_exploration_map': self.counts_map,
+                'reset': reset,
+                'x': x,
+                'y': y,
+                'map': map_n,
+                'pokemon': pokemon,
+            }
+
+        if self.verbose:
+            print(
+                f'steps: {self.time}',
+                f'exploration reward: {exploration_reward}',
+                f'level_Reward: {level_reward}',
+                f'healing: {healing_reward}',
+                f'death: {death_reward}',
+                f'op_level: {opponent_level_reward}',
+                f'badges reward: {badges_reward}',
+                f'event reward: {event_reward}',
+                f'money: {money}',
+                f'ai reward: {reward}',
+                f'Info: {info}',
+            )
+
+        return self.render()[::2, ::2], reward, done, done, info
